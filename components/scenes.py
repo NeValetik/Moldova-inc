80ad59163000701878a5a2569980c310225b2a2c
Timur <timurcravtov04@gmail.com>
2024-03-31 21:12:09 +0300

    requirements

--- components/scenes.py
+++ components/scenes.py
@@ -556,13 +556,13 @@ class Timer:
     def display_timer(cls, window):
         time = cls.get_time()
 
-        border_rect = pygame.Rect(990, 100, 140, 20)
+        border_rect = pygame.Rect(1040, 20, 100, 20)
         pygame.draw.rect(window, (255, 255, 255), border_rect, border_radius=40)
 
         font = pygame.font.Font("assets/font/evil-empire.ttf", 20)
         text = font.render(time, True, (0, 0, 0))
         text_rect = text.get_rect()
-        text_rect.center = (1080, 110)
+        text_rect.center = (1090, 30)
         window.blit(text, text_rect)
 
     @classmethod
