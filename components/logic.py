d69ef7f9f8472b6ceee2257903018792f7a02253
Nevalet <snowchug@gmail.com>
2024-05-10 17:43:43 +0300

    Money is now printed on contract

--- components/logic.py
+++ components/logic.py
@@ -128,7 +128,7 @@ class Graph(nx.Graph):
         self.check_remove_invalid_by_date_contract()
         while len(Country.contracts) != 0:
             contract = Country.contracts[0]
-            weight = self.income(contract[1])
+            weight = income(contract[1])
             Graph.total_income += weight
             self.add_weighted_edges_from([(contract[0], contract[1], weight)])
             del Country.contracts[0]
@@ -145,8 +145,6 @@ class Graph(nx.Graph):
                     self.add_weighted_edges_from([(country.moldova, country, 0)])
                     self.countries_init.pop(0)
 
-    def income(self, contract):
-        return Wine.naturality * contract.naturality_coef + Wine.advertisment * contract.advertisment_coef + Wine.taste * contract.taste_coef
 
     def check_remove_invalid_by_date_contract(self):
         for u, v in self.edges():
